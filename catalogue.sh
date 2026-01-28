593d400d05834cac6aa62ec03f3a56e2b40afa95
Dillshad685 <dillshadb2@gmail.com>
2026-01-29 02:48:22 +0530

    commit

--- catalogue.sh
+++ catalogue.sh
@@ -2,37 +2,38 @@
 
 #!/bin/bash
 
-=========================== to add colors =========================================
+#=========================== to add colors =========================================#
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-======================= to create log file ========================================
+#======================= to create log file ========================================#
 
 LOG_FOLDER="/var/log/roboshop-shell"
-SCRIPT_NAME=$(echo "$0" | cut d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME.log
 SCRIPT_DIR=$PWD
 echo "log file path: $LOG_FILE"
 
 
 mkdir -p $LOG_FILE  &>>$LOG_FILE
-echo "Script started executing from $(date +%s)" | tee -a $LOG_FILE 
+echo "Script started executing from $(date)" | tee -a $LOG_FILE 
 
 if [ $USERID -ne 0 ]; then
     echo "run with super user"
     exit 1
 fi 
 
-============nodejs====================
+
+#============nodejs====================#
 
 dnf module disable nodejs -y &>>$LOG_FILE
 dnf module enable nodejs:20 -y  &>>$LOG_FILE
 dnf module install nodejs -y &>>$LOG_FILE
 echo -e "installing nodejs is $G success $N"
 
-===========user creation===============
+#===========user creation===============#
 id roboshop 
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>LOG_FILE
@@ -40,7 +41,7 @@ else
     echo -e "user already exist $Y SKIPPING $N"
 fi
 
-==================APPLICATION SETUP===============================
+#==================APPLICATION SETUP===============================#
 mkdir -p /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 cd /app &>>$LOG_FILE
@@ -49,10 +50,10 @@ npm install &>>$LOG_FILE
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
 systemctl daemon-reload 
 systemctl enable catalogue 
-sytemctl start catalogue 
+systemctl start catalogue 
 echo -e "catalogue application deployment $G success $N"
 
-=============================MONGO DB SETUP=============================
+#=============================MONGO DB SETUP=============================#
 
 cp $SCRIPT_DIR/mongodb.service /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mogosh -y &>>LOG_FILE
