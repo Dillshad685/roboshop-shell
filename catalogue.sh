1a5d129f4fe42885ff4adb1a5e4327f35309a436
Dillshad685 <dillshadb2@gmail.com>
2026-02-01 23:35:05 +0530

    commit

--- catalogue.sh
+++ catalogue.sh
@@ -10,12 +10,12 @@ N="\e[0m"
 
 #======================= to create log file ========================================#
 
-LOG_FOLDER="/var/log/roboshop-shell"
+LOGS_FOLDER="/var/log/roboshop-shell"
 echo "$0"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
-mkdir -p $LOG_FOLDER  &>>$LOG_FILE
+mkdir -p $LOGS_FOLDER  &>>$LOG_FILE
 echo "log file path: $LOG_FILE"
 echo "Script started executing from $(date)" | tee -a $LOG_FILE 
 
@@ -33,38 +33,38 @@ dnf module enable nodejs:20 -y  &>>$LOG_FILE
 dnf install nodejs -y &>>$LOG_FILE
 echo -e "installing nodejs is $G success $N"
 
-#===========user creation===============#
-id roboshop 
-if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>LOG_FILE
-else
-    echo -e "user already exist $Y SKIPPING $N"
-fi
+# #===========user creation===============#
+# id roboshop 
+# if [ $? -ne 0 ]; then
+#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>LOG_FILE
+# else
+#     echo -e "user already exist $Y SKIPPING $N"
+# fi
 
-#==================APPLICATION SETUP===============================#
-mkdir -p /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-cd /app &>>$LOG_FILE
-rm -rf /app/* &>>$LOG_FILE
-cd /app 
-unzip /tmp/catalogue.zip 
-cd /app 
-npm install &>>$LOG_FILE
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
-systemctl daemon-reload 
-systemctl enable catalogue 
-systemctl start catalogue 
-echo -e "catalogue application deployment $G success $N"
+# #==================APPLICATION SETUP===============================#
+# mkdir -p /app
+# curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+# cd /app &>>$LOG_FILE
+# rm -rf /app/* &>>$LOG_FILE
+# cd /app 
+# unzip /tmp/catalogue.zip 
+# cd /app 
+# npm install &>>$LOG_FILE
+# cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
+# systemctl daemon-reload 
+# systemctl enable catalogue 
+# systemctl start catalogue 
+# echo -e "catalogue application deployment $G success $N"
 
-#=============================MONGO DB SETUP=============================#
+# #=============================MONGO DB SETUP=============================#
 
-cp $SCRIPT_DIR/mongodb.service /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mogosh -y &>>LOG_FILE
-INDEX=$(mongosh mongodb.dillshad.space --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
-    mongosh --host mongodb.dillshad.space </app/db/master-data.js
-else
-    echo -e "products already present $Y SKIPPING $N"
-fi
+# cp $SCRIPT_DIR/mongodb.service /etc/yum.repos.d/mongo.repo
+# dnf install mongodb-mogosh -y &>>LOG_FILE
+# INDEX=$(mongosh mongodb.dillshad.space --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+# if [ $INDEX -le 0 ]; then
+#     mongosh --host mongodb.dillshad.space </app/db/master-data.js
+# else
+#     echo -e "products already present $Y SKIPPING $N"
+# fi
 
-systemctl restart catalogue &>>LOG_FILE
+# systemctl restart catalogue &>>LOG_FILE
