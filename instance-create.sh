3aa26a0f805a3be560da1ac0cf8e7e82e447db6b
Dillshad685 <dillshadb2@gmail.com>
2026-02-06 12:20:38 +0530

    commit

--- instance-create.sh
+++ instance-create.sh
@@ -6,24 +6,21 @@ ZONE_ID="Z0375645LTAC4FZXZR6K"
 DOMAIN_NAME="dillshad.space"
 
 for instance in $@ 
-do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)  #to create EC2 instance using shell here instance name which we give dynamically is stored in $instance
-
-        #to get private IP
-    if [ $instance != frontend ]; then
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
-    else
+    else 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
-
-    echo "$instance: $IP"
+    echo "$instance=$IP"
 
      aws route53 change-resource-record-sets \
    --hosted-zone-id $ZONE_ID \
    --change-batch '
-    {
+   {
      "Comment": "Updating record set"
      ,"Changes": [{
        "Action"              : "UPSERT"
