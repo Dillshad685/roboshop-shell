5b41f52945c625ddeb72252f03efdd2ea6faf903
Dillshad685 <dillshadb2@gmail.com>
2026-02-06 15:58:44 +0530

    commit

---
+++ mysql.sh
@@ -0,0 +1,43 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-shell"
+SCRIPT_PATH=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_PATH.log"
+mkdir -p $LOGS_FOLDER
+echo "$LOG_FILE"
+
+echo -e "script execution start time : $(date)" | tee -a $LOG_FILE
+START_TIME=$(date +%s)
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R RUN WITH SUPER USER $N"
+    exit 1 
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $1.. FAILED $N"
+        exit 1
+    else
+        echo -e "$G $2 .. SUCCESS $N"
+    fi
+}
+
+dnf module install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "INSTALLING MYSQL"
+systemctl enable mysql-server &>>$LOG_FILE
+VALIDATE $? "ENABLING MYSQL"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "STARTING MYSQL"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "root pwd set"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo "code executed in $TOTAL_TIME seconds"
+
